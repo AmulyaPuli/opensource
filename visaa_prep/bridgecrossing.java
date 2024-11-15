f612588c0ba10e381800b4f0151ab5454373c371
AmulyaPuli <163538541+AmulyaPuli@users.noreply.github.com>
2024-11-15 17:04:34 +0530

    Create bridgecrossing.java

---
+++ visaa_prep/bridgecrossing.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        int z = sc.nextInt();
+        int a = z-y;
+        int b = a/x;
+        System.out.println(b);
+            
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    }
+}
